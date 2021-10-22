ba2a3744c34d1d7a36185e06e489553818528566
Annette Pohl <annette.pohl@brickmakers.de>
2021-10-22 18:31:45 +0200

    updated npm packages, added delay to one scenario

--- spec/src/ScenarioUtils.js
+++ spec/src/ScenarioUtils.js
@@ -130,8 +130,8 @@ module.exports = {
 
 	defaultTimeout: 30 * 1000,
 	
-	browserName: "firefox"
-	//browserName: "chrome"
+	//browserName: "firefox"
+	browserName: "chrome"
 	//browserName: "safari" // execute: safaridriver --enable
 
 }
