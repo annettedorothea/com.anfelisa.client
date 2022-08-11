271c8921fe52e49814123e95b56b67929a016925
Annette Pohl <annette_pohl@web.de>
2022-08-11 16:18:25 +0200

    changed button order

--- es6/gen/components/rootContainer/mainView/authorView/categoryTree/ButtonsContainer.js
+++ es6/gen/components/rootContainer/mainView/authorView/categoryTree/ButtonsContainer.js
@@ -11,14 +11,14 @@ import { Buttons } from "../../../../../../src/components/rootContainer/mainView
 import { Back } from "../../../../../../src/components/rootContainer/mainView/authorView/categoryTree/buttons/Back";
 import { New } from "../../../../../../src/components/rootContainer/mainView/authorView/categoryTree/buttons/New";
 import { Edit } from "../../../../../../src/components/rootContainer/mainView/authorView/categoryTree/buttons/Edit";
-import { Invite } from "../../../../../../src/components/rootContainer/mainView/authorView/categoryTree/buttons/Invite";
 import { Delete } from "../../../../../../src/components/rootContainer/mainView/authorView/categoryTree/buttons/Delete";
+import { Invite } from "../../../../../../src/components/rootContainer/mainView/authorView/categoryTree/buttons/Invite";
 import { Reverse } from "../../../../../../src/components/rootContainer/mainView/authorView/categoryTree/buttons/Reverse";
 import { routeToDefault } from "../../../../../common/ActionFunctions";
 import { newCategoryClick } from "../../../../../category/ActionFunctions";
 import { editCategoryClick } from "../../../../../category/ActionFunctions";
-import { inviteUserClick } from "../../../../../category/ActionFunctions";
 import { deleteCategoryClick } from "../../../../../category/ActionFunctions";
+import { inviteUserClick } from "../../../../../category/ActionFunctions";
 import { createReverseBox } from "../../../../../category/ActionFunctions";
 
 
@@ -28,8 +28,8 @@ export const ButtonsContainer = ( props ) => {
 		<Back {...props.back }   onClick={routeToDefault}  />
 		<New {...props.new }  selectedCategory={props.selectedCategory} rootCategory={props.rootCategory}  onClick={newCategoryClick}  />
 		<Edit {...props.edit }  selectedCategory={props.selectedCategory} rootCategory={props.rootCategory}  onClick={editCategoryClick}  />
-		<Invite {...props.invite }  selectedCategory={props.selectedCategory} rootCategory={props.rootCategory}  onClick={inviteUserClick}  />
 		<Delete {...props.delete }  selectedCategory={props.selectedCategory} rootCategory={props.rootCategory}  onClick={deleteCategoryClick}  />
+		<Invite {...props.invite }  selectedCategory={props.selectedCategory} rootCategory={props.rootCategory}  onClick={inviteUserClick}  />
 		<Reverse {...props.reverse }  selectedCategory={props.selectedCategory} rootCategory={props.rootCategory} reverseBoxExists={props.reverseBoxExists}  onClick={createReverseBox}  />
 	</Buttons> 
 }
